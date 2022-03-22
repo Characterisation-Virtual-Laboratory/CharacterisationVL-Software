2294d88fd6340419c5024885ea00d71d77f2f6fb
Edan Scriven <edan.scriven@uqconnect.edu.au>
2022-03-23 09:29:52 +1000

    Added a script to build a container from a dev branch as part of a github action.

---
+++ build-a-branch.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Build-A-Branch
+#
+# This script is meant to be executed as part of a GitHub action.
+# It will build a Singularity container from a new recipe pushed to the repo
+#
+# Assumptions:
+# You are running this script from the base of the repository.
+# You have checked out the new app's branch and pulled it.
+
+
+# Preserve starting directory in case of multiple builds in one commit
+# TODO: Make the container and log directories input arguments
+BASEDIR=$PWD
+CONTAINERDIR="/mnt/volume/test-containers"
+LOGDIR="/mnt/volume/logs"
+
+# Find out new and changed files via git diff-tree
+for file in `git diff-tree --no-commit-id --name-only`
+do
+	# Only operate on Singularity recipes, adopting the convention that they are all named "Singularity.appname"
+	if [[ $file =~ '^Singularity\.' ]]
+	then
+		# Call the resulting container and log the same as the app name
+		CONTAINERNAME=${file#Singularity.}
+		cd dirname($file)
+		# TODO: consider the security implications of not restricting commands in sudoers or setuid the script
+		sudo singularity build $CONTAINERDIR/$CONTAINERNAME.sif 2>&1 > $LOGDIR/$CONTAINERNAME.log
+		# Return to the repo's base directory to potentially build the next recipe
+		cd $BASEDIR
+	fi
+done
